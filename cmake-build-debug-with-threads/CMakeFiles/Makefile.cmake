b4c79467e301b470d9769c2aca1eb175e5d61784
Aaron Fortner <afortner@islander.tamucc.edu>
2021-04-20 22:56:27 -0500

    step 2

--- cmake-build-debug-with-threads/CMakeFiles/Makefile.cmake
+++ cmake-build-debug-with-threads/CMakeFiles/Makefile.cmake
@@ -10,14 +10,17 @@ set(CMAKE_MAKEFILE_DEPENDS
   "C:/Program Files/JetBrains/CLion 2020.3.2/bin/cmake/win/share/cmake-3.19/Modules/CMakeCInformation.cmake"
   "C:/Program Files/JetBrains/CLion 2020.3.2/bin/cmake/win/share/cmake-3.19/Modules/CMakeCXXInformation.cmake"
   "C:/Program Files/JetBrains/CLion 2020.3.2/bin/cmake/win/share/cmake-3.19/Modules/CMakeCommonLanguageInclude.cmake"
+  "C:/Program Files/JetBrains/CLion 2020.3.2/bin/cmake/win/share/cmake-3.19/Modules/CMakeDetermineRCCompiler.cmake"
   "C:/Program Files/JetBrains/CLion 2020.3.2/bin/cmake/win/share/cmake-3.19/Modules/CMakeExtraGeneratorDetermineCompilerMacrosAndIncludeDirs.cmake"
   "C:/Program Files/JetBrains/CLion 2020.3.2/bin/cmake/win/share/cmake-3.19/Modules/CMakeFindCodeBlocks.cmake"
   "C:/Program Files/JetBrains/CLion 2020.3.2/bin/cmake/win/share/cmake-3.19/Modules/CMakeGenericSystem.cmake"
   "C:/Program Files/JetBrains/CLion 2020.3.2/bin/cmake/win/share/cmake-3.19/Modules/CMakeInitializeConfigs.cmake"
   "C:/Program Files/JetBrains/CLion 2020.3.2/bin/cmake/win/share/cmake-3.19/Modules/CMakeLanguageInformation.cmake"
+  "C:/Program Files/JetBrains/CLion 2020.3.2/bin/cmake/win/share/cmake-3.19/Modules/CMakeRCCompiler.cmake.in"
   "C:/Program Files/JetBrains/CLion 2020.3.2/bin/cmake/win/share/cmake-3.19/Modules/CMakeRCInformation.cmake"
   "C:/Program Files/JetBrains/CLion 2020.3.2/bin/cmake/win/share/cmake-3.19/Modules/CMakeSystemSpecificInformation.cmake"
   "C:/Program Files/JetBrains/CLion 2020.3.2/bin/cmake/win/share/cmake-3.19/Modules/CMakeSystemSpecificInitialize.cmake"
+  "C:/Program Files/JetBrains/CLion 2020.3.2/bin/cmake/win/share/cmake-3.19/Modules/CMakeTestRCCompiler.cmake"
   "C:/Program Files/JetBrains/CLion 2020.3.2/bin/cmake/win/share/cmake-3.19/Modules/Compiler/CMakeCommonCompilerMacros.cmake"
   "C:/Program Files/JetBrains/CLion 2020.3.2/bin/cmake/win/share/cmake-3.19/Modules/Compiler/GNU-C.cmake"
   "C:/Program Files/JetBrains/CLion 2020.3.2/bin/cmake/win/share/cmake-3.19/Modules/Compiler/GNU-CXX.cmake"
@@ -46,6 +49,7 @@ set(CMAKE_MAKEFILE_OUTPUTS
 
 # Byproducts of CMake generate step:
 set(CMAKE_MAKEFILE_PRODUCTS
+  "CMakeFiles/3.19.2/CMakeRCCompiler.cmake"
   "CMakeFiles/CMakeDirectoryInformation.cmake"
   )
 
